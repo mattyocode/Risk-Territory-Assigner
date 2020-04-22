769bab1e21e403e3117ede1648d99a40ac521526
Squareben1 <bengittins@live.com>
2020-04-22 11:05:37 +0100

    splitting array into x arrays

--- lib/assigner.py
+++ lib/assigner.py
@@ -5,5 +5,12 @@ class Assigner:
     # def __init__(self):
     #     self.list = []
     
-    def randomise(self, arr, number):
+    def randomise(self, arr):
         return random.sample(arr, len(arr))
+
+    def assign(self, arr, number): 
+        returned_array = []
+        splitter = len(arr) / number
+        for i in range(0, len(arr), splitter): #the last arg determines how many items in each returned
+            returned_array.append(arr[i:i + splitter])
+        return returned_array
--- tests/assigner_unit_test.py
+++ tests/assigner_unit_test.py
@@ -2,9 +2,18 @@ import pytest
 import random
 from lib import Assigner
 
-array = ['Kamchatka', 'Japan', 'Eastern Austrlia', 'Alaska', 'Northwest Territory']
+array = ['Kamchatka', 'Japan', 'Eastern Australia', 'Alaska', 'Northwest Territory', 'Iceland']
+
 
 subject = Assigner()
 
-def test_split():
-    assert subject.randomise(array, 2) != array
+def test_randomise_returns_shuffled_array():
+    assert subject.randomise(array) != array
+
+def test_assign_shuffled_arrays_to_2_players():
+    print('subject.assign(array, 2)', subject.assign(array, 2))
+    assert subject.assign(array, 2) == [['Kamchatka', 'Japan', 'Eastern Australia'], ['Alaska', 'Northwest Territory', 'Iceland']]
+
+def test_assign_shuffled_arrays_to_3_players():
+    print('subject.assign(array, 3)', subject.assign(array, 3))
+    assert subject.assign(array, 3) == [['Kamchatka', 'Japan'], ['Eastern Australia', 'Alaska'], ['Northwest Territory', 'Iceland']]
