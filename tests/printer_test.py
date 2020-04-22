f1339da1bcd49305e46a6828a363ef48b52e15dd
Squareben1 <bengittins@live.com>
2020-04-22 22:36:54 +0100

    Prints lists up to 6 players

--- tests/printer_test.py
+++ tests/printer_test.py
@@ -1,3 +1,21 @@
 import pytest
 from lib import Printer
+from lib import Assigner
 
+full_list = ['Alaska', 'Northwest Territory', 'Greenland', 'Alberta', 'Ontario', 'Quebec', 'Western United States', 'Eastern United States', 'Central America', 'Venezuela', 'Peru', 'Brazil', 'Argentina', 'North Africa', 'Egypt', 'East Africa', 'Congo', 'South Africa', 'Madagascar', 'Iceland', 'Scandinavia', 'Ukraine', 'Great Britian', 'Northern Europe', 'Southern Europe', 'Western Europe', 'Indonesia', 'New Guinea', 'Western Australia', 'Eastern Australia', 'Siam', 'India', 'China', 'Mongolia', 'Japan', 'Irkutsk', 'Yakutsk', 'Kamchatka', 'Siberia', 'Afghanistan', 'Ural', 'Middle East']
+array = ['Kamchatka', 'Japan', 'Eastern Australia', 'Alaska', 'Northwest Territory', 'Iceland']
+test_array = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+
+subject = Printer()
+assigner = Assigner()
+# scrambled_ters = assigner.randomise(array)
+assigned_array = assigner.chunk_territories(array, 2)
+assigned_test_array = assigner.chunk_territories(test_array, 6)
+
+class TestPrinter:
+    def test_print_string_2_player(self):
+        assert subject.print_ters(assigned_array) == 'Player 1:\nKamchatka\nEastern Australia\nNorthwest Territory\nPlayer 2:\nJapan\nAlaska\nIceland'
+  
+    def test_print_string_6_player(self):
+        print('subject.print_ters(assigned_test_array)', subject.print_ters(assigned_test_array))
+        assert subject.print_ters(assigned_test_array) == 'Player 1:\n1\n7\nPlayer 2:\n2\n8\nPlayer 3:\n3\n9\nPlayer 4:\n4\n10\nPlayer 5:\n5\n11\nPlayer 6:\n6\n12'
